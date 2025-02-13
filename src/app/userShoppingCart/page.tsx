346e44546c84851a49b9c70954c7589ca43d7d5f
Saba Zarei <sabaz3311@gmail.com>
2025-02-13 13:08:02 +0330

    small changes

--- src/app/userShoppingCart/page.tsx
+++ src/app/userShoppingCart/page.tsx
@@ -6,35 +6,35 @@ import Image from "next/image";
 const UserShoppingCartPage = () => {
   return (
     <>
-      <div className="grid grid-cols-7">
+      <div className="grid grid-cols-7 max-sm:grid-cols-4">
         
-        <div className=" mr-20 col-span-5 ">
+        <div className=" mr-20 col-span-5 mt-36 max-sm:col-span-4">
         <CartTabs />
         </div>
 
 
-        <div className=" col-span-2  ">
-         <CostCart/> 
+        <div className=" col-span-2 mt-36 ">
+         {/* <CostCart/>  */}
          <div className="mr-11 mt-9">
           <div className="flex mb-3 items-center">
 
             <div>
               <Image width={25} height={25} src={'/imgs/return-policy.svg'} alt="return"/>
             </div>
-            <p className="mr-2 font-bold text-sm">هفت روز ضمانت تعویض</p>
+            <p className="mr-2 font-bold text-[13px]">هفت روز ضمانت تعویض</p>
 
           </div>
           <div className="flex mb-3 items-center">
           <div>
               <Image width={25} height={25} src={'/imgs/payment-terms.svg'} alt="payment" />
             </div>
-            <p className="mr-2 font-bold text-sm"> پرداخت در محل با کارت بانکی</p>
+            <p className="mr-2 font-bold text-[13px]"> پرداخت در محل با کارت بانکی</p>
           </div>
           <div className="flex mb-3 items-center">
           <div>
               <Image width={25} height={25} src={'/imgs/delivery.svg'} alt="delivery"/>
             </div>
-            <p className="mr-2 font-bold text-sm "> تحویل اکسپرس</p>
+            <p className="mr-2 font-bold text-[13px] "> تحویل اکسپرس</p>
           </div>
          </div>
         </div>
