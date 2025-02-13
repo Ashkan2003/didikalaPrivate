ae3b31cafbf6037f3dd67824455edeb8da067c37
ashkan <ashkanpc2003@gmail.com>
2025-02-13 12:30:30 +0330

    setup-bastSellingProductsSection-step-1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import AmazingOfferSection from "@/components/main-page/AmazingOfferSection";
+import BestSellingProductsSection from "@/components/main-page/BestSellingProductsSection";
 import BigBannersSection from "@/components/main-page/BigBannersSection";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <div className="space-y-16 pt-6">
       <BigBannersSection />
       <AmazingOfferSection />
+      <BestSellingProductsSection />
     </div>
   );
 }
