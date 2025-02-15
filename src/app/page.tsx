458569d960e505b18e20279b79cd0bd61e51febd
ashkan <ashkanpc2003@gmail.com>
2025-02-15 11:02:38 +0330

    setup small-banner-section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
 import AmazingOfferSection from "@/components/main-page/AmazingOfferSection";
 import BestSellingProductsSection from "@/components/main-page/BestSellingProductsSection";
 import BigBannersSection from "@/components/main-page/BigBannersSection";
+import { MediumBanner } from "@/components/main-page/MediumBannerSection";
+import SmallBannerSection from "@/components/main-page/SmallBannerSection";
 
 export default function Home() {
   return (
     <div className="space-y-16 pt-6">
       <BigBannersSection />
-      <AmazingOfferSection />
+      <AmazingOfferSection bgColor="red" />
       <BestSellingProductsSection />
+      <MediumBanner />
+      <AmazingOfferSection bgColor="blue" />
+      <SmallBannerSection />
     </div>
   );
 }
