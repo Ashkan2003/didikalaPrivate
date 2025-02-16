5252712df07167ffa2d9cd61c9f292bab348206a
ashkan <ashkanpc2003@gmail.com>
2025-02-16 10:24:37 +0330

    setup brand-slider-section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import AmazingOfferSection from "@/components/main-page/AmazingOfferSection";
 import BestSellingProductsSection from "@/components/main-page/BestSellingProductsSection";
 import BigBannersSection from "@/components/main-page/BigBannersSection";
+import BrandSliderSection from "@/components/main-page/BrandSliderSection";
 import CategoryCounterSection from "@/components/main-page/CategoryCounterSection";
 import { MediumBanner } from "@/components/main-page/MediumBannerSection";
+import OurOfferSection from "@/components/main-page/OurOfferSection";
 import SmallBannerSection from "@/components/main-page/SmallBannerSection";
 
 export default function Home() {
@@ -15,6 +17,9 @@ export default function Home() {
       <AmazingOfferSection bgColor="blue" />
       <SmallBannerSection />
       <CategoryCounterSection />
+      <BestSellingProductsSection />
+      <OurOfferSection />
+      <BrandSliderSection />
     </div>
   );
 }
